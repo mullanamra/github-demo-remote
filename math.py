7877b7b89e01d612d3188ae515911dda390c47c6
mullanamra <98696036+mullanamra@users.noreply.github.com>
2022-02-01 11:07:08 +0530

    Implemented subtract fn from remote repo on main.

--- math.py
+++ math.py
@@ -4,7 +4,7 @@ def add(x,y):
 
 #subtract implementation	
 def subtract(x,y):
-	pass                  
+	return x-y                              #implemented subtract fn from remote repo on main branch                
 
 #multiply implementation
 def multiply(x,y):
